f6d517757b7d9f64b0d546d8b35ed8d07105a33f
So1n <qaz6803609@163.com>
2020-07-24 00:57:31 +0800

    Add, pkg

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pait",
+    version="0.1.0",
+    author="so1n",
+    author_email="so1n897046026@example.com",
+    description="Pait is a python api interface tool, which can also be called a python api interface type (type hint)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/so1n/pait",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
