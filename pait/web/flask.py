fa314bb1a60188c197bbc0b56126abbab37efeb0
So1n <qaz6803609@163.com>
2020-08-05 21:03:45 +0800

    Modify, fix, flask type error

--- pait/web/flask.py
+++ pait/web/flask.py
@@ -2,14 +2,17 @@ from functools import partial
 from typing import Any, Callable, Dict, Mapping, Tuple
 
 from flask import request, Request
+from werkzeug.datastructures import EnvironHeaders, ImmutableMultiDict
+
 from pait.web.base import BaseAsyncWebDispatch
 from pait.verify import sync_params_verify
 
 
 class FlaskDispatch(BaseAsyncWebDispatch):
     RequestType = Request
-    FormType = Request.form
+    FormType = ImmutableMultiDict
     FileType = Request.files
+    HeaderType = EnvironHeaders
 
     def __init__(
         self,
@@ -20,6 +23,7 @@ class FlaskDispatch(BaseAsyncWebDispatch):
     ):
         super().__init__(func, qualname, args, kwargs)
 
+        self.request = request
         self.path_dict: Dict[str, Any] = {}
         self.path_dict.update(self.request_kwargs)
 
@@ -35,7 +39,7 @@ class FlaskDispatch(BaseAsyncWebDispatch):
     def form(self) -> Request.form:
         return request.form
 
-    def header(self) -> Mapping:
+    def header(self) -> Request.headers:
         return request.headers
 
     def path(self) -> Dict[str, Any]:
