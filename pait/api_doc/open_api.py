5efe503928c37cedf553e4efc3f95f7e8cfb8dd5
So1n <qaz6803609@163.com>
2020-11-14 10:41:20 +0800

    Add, feat, app api doc feat file structure

---
+++ pait/api_doc/open_api.py
@@ -0,0 +1,42 @@
+open_api_dict = {
+    "openapi": "3.0.2",
+    "info": {
+        "title": "pait test open api",
+        "description": "测试pait的openapi",
+        "version": "0.1.0",
+        "termsOfService": "",
+        "contact": {
+            "name": "Pait Api",
+            "url": "http://www.example.support",
+            "email": "pait@example.com"
+        },
+        "license": {
+            "name": "Apache 2.0",
+            "url": "http://www.apache.org/licenses/LICENSE-2.0.html"
+        }
+    },
+    "servers": {
+        "url": "/",
+        "description": "test pait server"
+    },
+    "tags": {
+        "name": "test",
+        "description": "about test api",
+        "externalDocs": {
+            "description": "external docx",
+            "url": " http://example.com"
+        }
+    },
+    "paths": {
+
+    },
+    "components": {
+
+    },
+    "security": {
+
+    },
+    "externalDocs": {
+
+    }
+}
