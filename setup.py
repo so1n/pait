87d04a4e9d790c728855b6ae7aa615cab1a3de1b
so1n <qaz6803609@163.com>
2021-06-07 01:49:25 +0800

    Modify, build, version 0.5.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pait",
-    version="0.5.4",
+    version="0.5.5",
     author="so1n",
     author_email="so1n897046026@gmail.com",
     description=(
