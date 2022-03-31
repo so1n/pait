d9fee41ca5397523cfb51f338f6a5c98a0969381
so1n <qaz6803609@163.com>
2022-03-31 14:37:56 +0800

    Modify, Feature, starlette load_app func support Mount route

--- pait/app/starlette/_load_app.py
+++ pait/app/starlette/_load_app.py
@@ -1,9 +1,10 @@
 import inspect
-from typing import Callable, Dict, Type, Union
+import logging
+from typing import Callable, Dict, Optional, Type, Union
 
+from starlette import routing
 from starlette.applications import Starlette
 from starlette.endpoints import HTTPEndpoint
-from starlette.routing import Route
 
 from pait.g import pait_data
 from pait.model.core import PaitCoreModel
@@ -14,35 +15,50 @@ from ._app_helper import AppHelper
 __all__ = ["load_app"]
 
 
+def _load_route(
+    *, route: routing.Route, _pait_data: Dict[str, PaitCoreModel], project_name: str, prefix_path: Optional[str] = None
+) -> None:
+    path: str = route.path
+    if prefix_path:
+        path = prefix_path + path
+    openapi_path: str = path
+    method_set: set = route.methods or set()
+    route_name: str = route.name
+    endpoint: Union[Callable, Type] = route.endpoint
+    pait_id: str = getattr(route.endpoint, "_pait_id", None)
+    if not pait_id and inspect.isclass(endpoint) and issubclass(endpoint, HTTPEndpoint):  # type: ignore
+        for method in http_method_tuple:
+            method_endpoint = getattr(endpoint, method, None)
+            if not method_endpoint:
+                continue
+            method_set = {method}
+            pait_id = getattr(method_endpoint, "_pait_id", None)
+            if not pait_id:
+                continue
+            pait_data.add_route_info(
+                AppHelper.app_name, pait_id, path, openapi_path, method_set, f"{route_name}.{method}", project_name
+            )
+            _pait_data[pait_id] = pait_data.get_pait_data(AppHelper.app_name, pait_id)
+    elif pait_id:
+        pait_data.add_route_info(AppHelper.app_name, pait_id, path, openapi_path, method_set, route_name, project_name)
+        _pait_data[pait_id] = pait_data.get_pait_data(AppHelper.app_name, pait_id)
+
+
 def load_app(app: Starlette, project_name: str = "") -> Dict[str, PaitCoreModel]:
     """Read data from the route that has been registered to `pait`"""
     _pait_data: Dict[str, PaitCoreModel] = {}
     for route in app.routes:
-        if not isinstance(route, Route):
-            # not support
-            continue
-        path: str = route.path
-        openapi_path: str = path
-        method_set: set = route.methods or set()
-        route_name: str = route.name
-        endpoint: Union[Callable, Type] = route.endpoint
-        pait_id: str = getattr(route.endpoint, "_pait_id", None)
-        if not pait_id and inspect.isclass(endpoint) and issubclass(endpoint, HTTPEndpoint):  # type: ignore
-            for method in http_method_tuple:
-                method_endpoint = getattr(endpoint, method, None)
-                if not method_endpoint:
-                    continue
-                method_set = {method}
-                pait_id = getattr(method_endpoint, "_pait_id", None)
-                if not pait_id:
-                    continue
-                pait_data.add_route_info(
-                    AppHelper.app_name, pait_id, path, openapi_path, method_set, f"{route_name}.{method}", project_name
+        if isinstance(route, routing.Route):
+            _load_route(route=route, _pait_data=_pait_data, project_name=project_name)
+        elif isinstance(route, routing.Mount):
+            for sub_route in route.routes:
+                _load_route(
+                    route=sub_route,  # type: ignore
+                    _pait_data=_pait_data,
+                    project_name=project_name,
+                    prefix_path=route.path,
                 )
-                _pait_data[pait_id] = pait_data.get_pait_data(AppHelper.app_name, pait_id)
-        elif pait_id:
-            pait_data.add_route_info(
-                AppHelper.app_name, pait_id, path, openapi_path, method_set, route_name, project_name
-            )
-            _pait_data[pait_id] = pait_data.get_pait_data(AppHelper.app_name, pait_id)
+        else:
+            logging.info(f"load_app func not support route:{route.__class__}")
+
     return _pait_data
