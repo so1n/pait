11b4f338f0a2f36ac4f38c0a4d45aeabf46e7983
so1n <qaz6803609@163.com>
2022-04-10 16:13:11 +0800

    Modify, util pkg like python asyncio

--- pait/util/__init__.py
+++ pait/util/__init__.py
@@ -1,11 +1,11 @@
 # flake8: noqa: F403
-from ._func_sig import *  # type: ignore  # flake8: noqa: F403
-from ._gen_tip import *  # type: ignore  # flake8: noqa: F403
-from ._i18n import *  # type: ignore  # flake8: noqa: F403
-from ._lazy_property import *  # type: ignore  # flake8: noqa: F403
-from ._pydantic_util import *  # type: ignore  # flake8: noqa: F403
-from ._types import *  # type: ignore  # flake8: noqa: F403
-from ._util import *  # type: ignore  # flake8: noqa: F403
+from ._func_sig import *  # type: ignore
+from ._gen_tip import *  # type: ignore
+from ._i18n import *  # type: ignore
+from ._lazy_property import *  # type: ignore
+from ._pydantic_util import *  # type: ignore
+from ._types import *  # type: ignore
+from ._util import *  # type: ignore
 
 __all__ = [
     _func_sig.__all__  # type: ignore
