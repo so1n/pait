9b1e259d06b0a8ec2be1b6d4ffc29ac6c8e3e1d1
so1n <qaz6803609@163.com>
2022-06-16 23:42:57 +0800

    Modify, Reset, reset swagger func design

--- pait/api_doc/html/__init__.py
+++ pait/api_doc/html/__init__.py
@@ -45,75 +45,36 @@ def get_swagger_ui_html(
     swagger_ui_standalone_preset: Optional[str] = None,
 ) -> str:
     if not swagger_ui_url:
-        swagger_ui_url = "https://unpkg.com/swagger-ui-dist@4.5.0/swagger-ui.css"
+        swagger_ui_url = "https://cdn.jsdelivr.net/npm/swagger-ui-dist@3/swagger-ui.css"
     if not swagger_ui_bundle:
-        swagger_ui_bundle = "https://unpkg.com/swagger-ui-dist@4.5.0/swagger-ui-bundle.js"
-    if not swagger_ui_standalone_preset:
-        swagger_ui_standalone_preset = "https://unpkg.com/swagger-ui-dist@4.5.0/swagger-ui-standalone-preset.js"
-
+        swagger_ui_bundle = "https://cdn.jsdelivr.net/npm/swagger-ui-dist@3/swagger-ui-bundle.js"
     return f"""
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta
-      name="description"
-      content="SwaggerUI"
-    />
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <link type="text/css" rel="stylesheet" href="{swagger_ui_url}">
+    <link rel="shortcut icon" href="https://static1.smartbear.co/swagger/media/assets/swagger_fav.png">
     <title>{title}</title>
-    <link rel="stylesheet" href="{swagger_ui_bundle}" />
-  </head>
-  <body>
-  <div id="swagger-ui"></div>
-  <script src="{swagger_ui_bundle}" crossorigin></script>
-  <script src="{swagger_ui_standalone_preset}" crossorigin></script>
-  <script>
-    window.onload = () => {{
-      window.ui = SwaggerUIBundle({{
+    </head>
+    <body>
+    <div id="swagger-ui">
+    </div>
+    <script src="{swagger_ui_bundle}"></script>
+    <!-- `SwaggerUIBundle` is now available on the page -->
+    <script>
+    const ui = SwaggerUIBundle({{
         url: '{open_api_json_url}',
         dom_id: '#swagger-ui',
         presets: [
-          SwaggerUIBundle.presets.apis,
-          SwaggerUIStandalonePreset
+        SwaggerUIBundle.presets.apis,
+        SwaggerUIBundle.SwaggerUIStandalonePreset
         ],
-        layout: "StandaloneLayout",
+        layout: "BaseLayout",
         deepLinking: true,
         showExtensions: true,
         showCommonExtensions: true
-      }});
-    }};
-  </script>
-  </body>
-</html>
-"""
-    # return f"""
-    # <!DOCTYPE html>
-    # <html>
-    # <head>
-    # <link type="text/css" rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swagger-ui-dist@3/swagger-ui.css">
-    # <link rel="shortcut icon" href="https://static1.smartbear.co/swagger/media/assets/swagger_fav.png">
-    # <title>{title}</title>
-    # </head>
-    # <body>
-    # <div id="swagger-ui">
-    # </div>
-    # <script src="https://cdn.jsdelivr.net/npm/swagger-ui-dist@3/swagger-ui-bundle.js"></script>
-    # <!-- `SwaggerUIBundle` is now available on the page -->
-    # <script>
-    # const ui = SwaggerUIBundle({{
-    #     url: '{open_api_json_url}',
-    #     dom_id: '#swagger-ui',
-    #     presets: [
-    #     SwaggerUIBundle.presets.apis,
-    #     SwaggerUIBundle.SwaggerUIStandalonePreset
-    #     ],
-    #     layout: "BaseLayout",
-    #     deepLinking: true,
-    #     showExtensions: true,
-    #     showCommonExtensions: true
-    # }})
-    # </script>
-    # </body>
-    # </html>
-    # """
+    }})
+    </script>
+    </body>
+    </html>
+    """
